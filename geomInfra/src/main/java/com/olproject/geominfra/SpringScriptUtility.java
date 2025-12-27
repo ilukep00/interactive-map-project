d45e46a353b599fb79d6b3d65110abf5090d62bb
ilukep00 <lukepastor23@gmail.com>
2025-12-27 18:30:01 +0100

    [interactive-map-project] Added dependency for executing sql scripts
    * Created utility class for this task

---
+++ geomInfra/src/main/java/com/olproject/geominfra/SpringScriptUtility.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.olproject.geominfra;
+
+import java.sql.Connection;
+import org.springframework.core.io.PathResource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+
+/**
+ *
+ * @author lukep
+ */
+public class SpringScriptUtility {
+    public static void runScript(String path, Connection connection) {
+        boolean continueOnError = false;
+        boolean ignoreFailedDrops = false;
+        String commentPrefix = "--";
+        String separator = ";";
+        String blockCommentStartDelimiter = "/*";
+        String blockCommentEndDelimiter = "*/";
+        
+        ScriptUtils.executeSqlScript(
+          connection,
+          new EncodedResource(new PathResource(path)),
+          continueOnError,
+          ignoreFailedDrops,
+          commentPrefix,
+          separator,
+          blockCommentStartDelimiter,
+          blockCommentEndDelimiter
+        );
+    }
+}
