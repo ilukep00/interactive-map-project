77dd45e4610b56a816d71d9e47b0472acf37104b
ilukep00 <lukepastor23@gmail.com>
2025-12-27 17:43:35 +0100

    [interactive-map-project] Created java process for creating the new database for the project
    * Edited .gitignore

---
+++ geomInfra/src/main/java/com/olproject/geominfra/GeomInfra.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.olproject.geominfra;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+/**
+ *
+ * @author lukep
+ */
+public class GeomInfra {
+
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Insert your postgres user name: ");
+        String username = scanner.nextLine();
+        
+        System.out.println("Insert your postgres password: ");
+        String password = scanner.nextLine();
+        
+        System.out.println("Insert your database name: ");
+        String dbName = scanner.nextLine();
+        
+        try (Connection conn = DriverManager.getConnection(url, username, password); 
+                Statement stmt = conn.createStatement()) {
+            
+            String sql = "CREATE DATABASE "+dbName;
+            stmt.execute(sql);
+            
+            System.out.println("Database created successfully!");
+            
+        } catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
